3be20a33f9e389da850bc6f66bd758b0d71ade7b
Zimmius <grybakov@usetech.ru>
2022-04-10 16:40:18 +0300

    Добавьте файлы проекта.

---
+++ autoit_desktop_addressbook_tests/appmanager/GroupHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace autoit_desktop_addressbook_tests
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialog();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(
+                    GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "GetText", "#0|#" + i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+            CloseGroupsDialog();
+            return list;
+        }
+
+        private void OpenGroupsDialog()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            aux.WinWait(GROUPWINTITLE);
+        }
+
+        public void AddGroup(GroupData newGroup)
+        {
+            OpenGroupsDialog();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialog();
+        }
+
+        public void RemoveGroup(int v)
+        {
+            OpenGroupsDialog();
+            SelectGroup(v);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+            aux.WinWait("Delete group");
+            aux.ControlClick("Delete group", "", "WindowsForms10.BUTTON.app.0.2c908d53");
+            CloseGroupsDialog();
+        }
+
+        public void SelectGroup(int v)
+        {
+            aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51", "Select", "#0|#"+ v, "");
+        }
+
+        private void CloseGroupsDialog()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinWaitActive(WINTITLE);
+        }
+    }
+}
---
+++ autoit_desktop_addressbook_tests/tests/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace autoit_desktop_addressbook_tests
+{
+    public class TestBase
+    {
+        public ApplicationManager app;
+
+        [OneTimeSetUp]
+        public void InitApplication()
+        {
+            app = new ApplicationManager();
+        }
+
+        [OneTimeTearDown]
+        public void StopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
